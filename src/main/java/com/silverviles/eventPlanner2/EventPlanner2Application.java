8f00a637bfac9b220ca1276bd4a10e134d1a4951
Silverviles <sudarakatharindu2001@gmail.com>
2023-12-13 08:48:43 +0530

    initial template commit

---
+++ src/main/java/com/silverviles/eventPlanner2/EventPlanner2Application.java
@@ -0,0 +1,13 @@
+package com.silverviles.eventPlanner2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanner2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlanner2Application.class, args);
+	}
+
+}
